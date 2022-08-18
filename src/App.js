f17a1f2e71e83532c56c7f1b2b7cfde632a28307
Zarry Myles <mahanth2709@gmail.com>
2022-08-18 21:59:28 +0530

    Search Filter bug fixed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
+
+// Components
+import CharList from "./components/CharList";
+import { SearchBox } from "./components/SearchBox";
 
 function App() {
+  const [chars, setChars] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const getCharRequest = async (search) => {
+    const url = `https://rickandmortyapi.com/api/character/?name=${search}`;
+
+    const res = await fetch(url);
+    const resJson = await res.json();
+
+    setChars(resJson.results);
+  };
+
+  useEffect(() => {
+    getCharRequest(search);
+  }, [search]);
+
   return (
-    <div className="flex min-h-screen items-center justify-center text-5xl uppercase">
-      hi!
+    <div className="flex bg-tahiti min-h-screen flex-col flex-wrap font-mono items-center justify-center ">
+      <div className="md:text-4xl flex text-xl justify-center items-center uppercase h-44">
+        Characters From Rick and Morty
+      </div>
+      <div>
+        <SearchBox search={search} setSearch={setSearch} />
+      </div>
+      <div className="flex flex-row bg-bubble-gum justify-between flex-wrap container">
+        <CharList chars={chars} />
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      tahiti: "#2DD4BF",
+      silver: "#ecebff",
+      "bubble-gum": "#ff77e9",
+    },
   },
   plugins: [],
 };
