9c97002987ae5aac2467148068e162d2b3495be9
Zarry Myles <mahanth2709@gmail.com>
2022-08-18 15:29:13 +0530

    Initialize App with TailwindCSS

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center text-5xl uppercase">
+      hi!
+    </div>
+  );
+}
+
+export default App;
